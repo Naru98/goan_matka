dfc351e611d005321f52005489a768e088f190b9
Naru98 <rawoolnar98@gmail.com>
2021-10-24 23:17:53 -0400

    fixed

--- application/views/admin/layout/footer.php
+++ application/views/admin/layout/footer.php
@@ -96,6 +96,51 @@
       })
     }
 
+    $("#login").validate({
+        rules: {
+          password: {
+            required: true,
+            minlength: 6,
+            maxlength: 16
+          }
+        },
+          submitHandler: function (form){
+          $('#overlay').show();
+          $('#error').text('');
+          $('#error').hide();
+          $('#success').text('');
+          $('#success').hide();
+          $.ajax({
+            url: SITE_URL+'admin/login/check',
+            data: $(form).serializeArray(),
+            type: 'POST',
+            success: function(data){
+              $('#overlay').hide();
+              const res = JSON.parse(data)
+              if(res.status==1)
+              {
+                $('#success').text(res.msg);
+                $('#success').show();
+                $("#success").scroll();
+                window.location.href= res.data.url;
+              }else{
+                $('#error').text(res.msg? res.msg : 'Error occurred! Please try again later.');
+                $('#error').show();
+                $("#error").scroll();
+              }
+            },
+            error:function (e){
+              $('#overlay').hide();
+              $('#error').text('Error occurred! Please try again later.');
+              $('#error').show();
+              $("#error").scroll();
+            }
+          })
+
+          }
+      });
+
+
     $("#addInfo").validate({
       rules: {
       },
