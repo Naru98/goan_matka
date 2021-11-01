82e6d8250f302abe49edbc06d18076f16721c434
Naru98 <rawoolnar98@gmail.com>
2021-10-31 23:50:43 -0400

    fixed

--- application/views/main.php
+++ application/views/main.php
@@ -87,47 +87,47 @@
 	    </div>
         <?php
             $live=[];
-            if($result)
-			{
-                $date = date('Y-m-d');
-                $time = date('H:i');
-				foreach ($result as $r) {
-                    if($r['day']== 7 || ($r['day'] >= date('w') && $r['day']!= 7 && date('w') != 0)  && false)
-                    {
-                        if(strtotime($r['date']) < strtotime($date) && $r['holiday'] == 0)
-                        {
-                            $min = (strtotime($r['open_time']) - strtotime($time)) / 60;
-                            if($min <= 10)
-                            {
-                                array_push($live,[
-                                    'name'=> $r['name'],
-                                    'data'=> 'Loading....'
-                                ]);
-                            }
-                        }else if(strtotime($r['date']) == strtotime($date) && $r['holiday'] == 0)
-                        {
-                            $min = (strtotime($r['open_time']) - strtotime($time)) / 60;
-                            if($min >= -10)
-                            {
-                                array_push($live,[
-                                    'name'=> $r['name'],
-                                    'data'=> $r['open_patti'].'-'.$r['open_ank']
-                                ]);
-                            }
+            // if($result)
+			// {
+            //     $date = date('Y-m-d');
+            //     $time = date('H:i');
+			// 	foreach ($result as $r) {
+            //         if($r['day']== 7 || ($r['day'] >= date('w') && $r['day']!= 7 && date('w') != 0)  && false)
+            //         {
+            //             if(strtotime($r['date']) < strtotime($date) && $r['holiday'] == 0)
+            //             {
+            //                 $min = (strtotime($r['open_time']) - strtotime($time)) / 60;
+            //                 if($min <= 10)
+            //                 {
+            //                     array_push($live,[
+            //                         'name'=> $r['name'],
+            //                         'data'=> 'Loading....'
+            //                     ]);
+            //                 }
+            //             }else if(strtotime($r['date']) == strtotime($date) && $r['holiday'] == 0)
+            //             {
+            //                 $min = (strtotime($r['open_time']) - strtotime($time)) / 60;
+            //                 if($min >= -10)
+            //                 {
+            //                     array_push($live,[
+            //                         'name'=> $r['name'],
+            //                         'data'=> $r['open_patti'].'-'.$r['open_ank']
+            //                     ]);
+            //                 }
 
-                            $min = (strtotime($r['close_time']) - strtotime($time)) / 60;
-                            if($min <= 10 || $min >= -10)
-                            {
-                                array_push($live,[
-                                    'name'=> $r['name'],
-                                    'data'=> $r['open_patti'].'-'.$r['open_ank'].''.$r['close_ank'].'-'.$r['close_patti']
-                                ]);
-                            }
+            //                 $min = (strtotime($r['close_time']) - strtotime($time)) / 60;
+            //                 if($min <= 10 || $min >= -10)
+            //                 {
+            //                     array_push($live,[
+            //                         'name'=> $r['name'],
+            //                         'data'=> $r['open_patti'].'-'.$r['open_ank'].''.$r['close_ank'].'-'.$r['close_patti']
+            //                     ]);
+            //                 }
 
-                        }
-                    }
-                }
-            }
+            //             }
+            //         }
+            //     }
+            // }
         ?>
         <!-- live result -->
         <?php if($live) { ?>
